3cb862bee9881bb986073912bc7ed720160a0f11
gavvahar <nihargavva@gmail.com>
2022-06-18 14:19:25 -0400

    changing it to be a switch statement

--- index.js
+++ index.js
@@ -11,8 +11,10 @@ function goLeft() {
   let secondAnswer = window.prompt(`You come across a stray cat. 
     It scampers off down a small hole, just large enough for you to crawl through. 
     Do you follow it, or continue on your path?`);
-  if (secondAnswer === "back") {
-    start();
+  switch (secondAnswer) {
+    case "back":
+      start();
+      break;
   }
 }
 
@@ -20,8 +22,10 @@ function goRight() {
   let secondAnswer = window.prompt(`You come across a snoring dragon. 
     On the other side of him, you see a shiny chest of treasure. Another path would 
     lead you away from the dragon all together. Which do you take?`);
-  if (secondAnswer === "back") {
-    start();
+  switch (secondAnswer) {
+    case "back":
+      start();
+      break;
   }
 }
 
